975f92ac4fd698951143a279a287f3ea3c5f7691
Kennedy Felipe <kennedyfelipe37@gmail.com>
2024-12-06 16:32:00 -0300

    atualização

--- php/usuario/login.php
+++ php/usuario/login.php
@@ -1,4 +1,62 @@
-::after, 
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GYMBRO LOGIN</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="../javascript/SCRIPT-LOGIN.js" defer></script>
+    <link rel="shortcut icon" href="../img/gymbrologoteste.png" type="image/x-icon">
+</head>
+
+<body>
+
+    <div class="box_login">
+        <h1 class="title">Bem vindo ao GYMBRO</h1>
+        <form id="loginForm" action="testeLogar.php" method="post">
+            <input id="login" type="email" name="email" placeholder="Digite seu e-mail..." required>
+            <input id="senha" type="password" name="password" placeholder="Digite sua senha..." required>
+            <i class="bi bi-eye-fill" id="btn-senha" onclick="mostrarSenha()"></i>
+            <div class="remember-forgot">
+                <label>
+                    <input type="checkbox">
+                    Lembrar minha senha
+                </label>
+                <a href="../html/esqueceuasenha.html">Esqueceu sua senha?</a>
+            </div>
+            <button type="submit">Entrar</button>
+            <p></p>
+            <button type="button" id="clearButton">Limpar</button>
+            <div class="divider"></div>
+            <p>Não tem uma conta?<a href="../html/cadastro.html" title="Criar uma conta">Cadastre-se</a></p>
+        </form>
+    </div>
+
+    <script>
+        const clearButton = document.getElementById('clearButton');
+
+        clearButton.addEventListener('click', function() {
+            document.getElementById('loginForm').reset();
+        });
+    </script>
+    <!-- VLibras Widget -->
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+    <!-- Fim VLibras Widget -->
+
+</body>
+</html>
+<style>
+    ::after, 
 ::before{
     padding: 0;
     margin: 0;
@@ -205,3 +263,5 @@ div i{
     font-weight: bold;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
+
+</style>
